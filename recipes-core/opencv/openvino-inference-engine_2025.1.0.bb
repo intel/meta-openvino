intel/meta-openvino

commit e3f1b36afa866017d04c60d0aeccd706a14ba0a2

openvino-inference-engine : backport patches to fix build issues with gcc-15
Signed-off-by: Yogesh Tyagi <yogesh.tyagi@intel.com>

recipes-core/opencv/openvino-inference-engine_2025.1.0.bb
@@ -23,6 +23,9 @@ SRC_URI = "git://github.com/openvinotoolkit/openvino.git;protocol=https;name=ope
            file://0004-fix-python-detection.patch \
            file://0004-Don-t-detect-arm-compute-library-version.patch \
            file://0001-intel_cpu-remove-executable-stack-flag-from-libopenv.patch \
+           file://0001-build-Fix-build-issue-using-Fedora-and-gcc-15-29880.patch \
+           file://0001-RecordProperty-serializes-ints-and-64-bit-ints-inclu.patch;patchdir=thirdparty/gtest/gtest \
+           file://0001-GPU-New-infra-for-OCL-CM-kernels-26985.patch \
            "
 
 SRCREV_openvino = "6fec06580ab8f60d1518420d17a02e8057a74349"

recipes-core/opencv/files/0001-GPU-New-infra-for-OCL-CM-kernels-26985.patch
@@ -1,0 +1,28 @@
+From 10f5087e3c580b738bbaeb203ca24f58359c8ff7 Mon Sep 17 00:00:00 2001
+From: Yogesh Tyagi <yogesh.tyagi@intel.com>
+Date: Thu, 15 May 2025 18:15:04 +0800
+Subject: [PATCH] Backport part of the patch to fix build issue with gcc-15
+
+Upstream-Status: Backport [https://github.com/openvinotoolkit/openvino/commit/53d26c22163d527778739c1d7042aaba617cd3be]
+
+Signed-off-by: Vladimir Paramuzov <vladimir.paramuzov@intel.com>
+
+---
+ src/plugins/intel_gpu/src/graph/include/primitive_inst.h | 1 -
+ 1 file changed, 1 deletion(-)
+
+diff --git a/src/plugins/intel_gpu/src/graph/include/primitive_inst.h b/src/plugins/intel_gpu/src/graph/include/primitive_inst.h
+index 4cbce44c22f..d4e3de3df7d 100644
+--- a/src/plugins/intel_gpu/src/graph/include/primitive_inst.h
++++ b/src/plugins/intel_gpu/src/graph/include/primitive_inst.h
+@@ -514,7 +514,6 @@ struct typed_primitive_impl : public primitive_impl {
+ 
+     using primitive_impl::primitive_impl;
+ 
+-private:
+     event::ptr execute(const std::vector<event::ptr>& event, primitive_inst& instance) override {
+         if (instance.type() != PType::type_id())
+             throw std::invalid_argument("Implementation type does not match primitive type");
+-- 
+2.37.3
+

recipes-core/opencv/files/0001-RecordProperty-serializes-ints-and-64-bit-ints-inclu.patch
@@ -1,0 +1,72 @@
+From 386c7665f5cf1f772f5877cdb32070d8052041a9 Mon Sep 17 00:00:00 2001
+From: Abseil Team <absl-team@google.com>
+Date: Mon, 7 Nov 2022 09:36:17 -0800
+Subject: [PATCH] RecordProperty serializes ints and 64-bit ints, including
+ size_ts
+
+PiperOrigin-RevId: 486685761
+Change-Id: I164d2646e65670d341dbf437ee571953c456677a
+
+Upstream-Status: Backport [https://github.com/google/googletest/commit/386c7665f5cf1f772f5877cdb32070d8052041a9]
+
+---
+ googletest/include/gtest/gtest.h            |  3 ++-
+ googletest/src/gtest.cc                     | 10 ++++------
+ googletest/test/gtest_xml_outfile2_test_.cc |  4 +++-
+ 3 files changed, 9 insertions(+), 8 deletions(-)
+
+Index: gtest/googletest/include/gtest/gtest.h
+===================================================================
+--- gtest.orig/googletest/include/gtest/gtest.h
++++ gtest/googletest/include/gtest/gtest.h
+@@ -53,6 +53,7 @@
+ #define GTEST_INCLUDE_GTEST_GTEST_H_
+ 
+ #include <cstddef>
++#include <cstdint>
+ #include <limits>
+ #include <memory>
+ #include <ostream>
+@@ -459,7 +460,7 @@ class GTEST_API_ Test {
+   // SetUp/TearDown method of Environment objects registered with Google
+   // Test) will be output as attributes of the <testsuites> element.
+   static void RecordProperty(const std::string& key, const std::string& value);
+-  static void RecordProperty(const std::string& key, int value);
++  static void RecordProperty(const std::string& key, int64_t value);
+ 
+  protected:
+   // Creates a Test object.
+Index: gtest/googletest/src/gtest.cc
+===================================================================
+--- gtest.orig/googletest/src/gtest.cc
++++ gtest/googletest/src/gtest.cc
+@@ -2268,12 +2268,10 @@ void Test::TearDown() {
+ void Test::RecordProperty(const std::string& key, const std::string& value) {
+   UnitTest::GetInstance()->RecordProperty(key, value);
+ }
+-
+-// Allows user supplied key value pairs to be recorded for later output.
+-void Test::RecordProperty(const std::string& key, int value) {
+-  Message value_message;
+-  value_message << value;
+-  RecordProperty(key, value_message.GetString().c_str());
++// We do not define a customary serialization except for integers,
++// but other values could be logged in this way.
++void Test::RecordProperty(const std::string& key, int64_t value) {
++  RecordProperty(key, (Message() << value).GetString());
+ }
+ 
+ namespace internal {
+Index: gtest/googletest/test/gtest_xml_outfile2_test_.cc
+===================================================================
+--- gtest.orig/googletest/test/gtest_xml_outfile2_test_.cc
++++ gtest/googletest/test/gtest_xml_outfile2_test_.cc
+@@ -39,5 +39,7 @@ class PropertyTwo : public testing::Test
+ };
+ 
+ TEST_F(PropertyTwo, TestSomeProperties) {
+-  RecordProperty("TestSomeProperty", 2);
++  // Validate we can write an unsigned size_t as a property
++  size_t prop_two = 2;
++  RecordProperty("TestSomeProperty", prop_two);
+ }

recipes-core/opencv/files/0001-build-Fix-build-issue-using-Fedora-and-gcc-15-29880.patch
@@ -1,0 +1,254 @@
+From c240db5ed317fdc94532dc55d356f18306ded996 Mon Sep 17 00:00:00 2001
+From: Pawel Raasz <pawel.raasz@intel.com>
+Date: Tue, 8 Apr 2025 19:35:47 +0200
+Subject: [PATCH] [build] Fix build issue using Fedora and gcc-15 (#29880)
+
+### Details:
+- Fix build issues for OpenVINO when using docker image with Fedora and
+gcc-15
+
+### Tickets:
+ - CVS-164531
+ - #29410
+
+---------
+Upstream-Status: Backport
+[https://github.com/openvinotoolkit/openvino/commit/c240db5ed317fdc94532dc55d356f18306ded996]
+
+Signed-off-by: Raasz, Pawel <pawel.raasz@intel.com>
+---
+ samples/cpp/common/format_reader/src/bmp.cpp         |  1 +
+ src/common/util/src/wstring_convert_util.cpp         | 10 +++++-----
+ src/core/dev_api/openvino/core/type/nf4.hpp          |  1 +
+ src/core/include/openvino/core/type/bfloat16.hpp     |  1 +
+ src/core/include/openvino/core/type/float16.hpp      |  1 +
+ src/core/include/openvino/core/type/float4_e2m1.hpp  |  1 +
+ src/core/include/openvino/core/type/float8_e4m3.hpp  |  1 +
+ src/core/include/openvino/core/type/float8_e5m2.hpp  |  1 +
+ src/core/include/openvino/core/type/float8_e8m0.hpp  |  1 +
+ .../include/openvino/reference/gru_cell.hpp          | 12 +++++-------
+ src/plugins/intel_cpu/src/nodes/rnn.cpp              |  3 ++-
+ .../intel_cpu/src/shape_inference/custom/one_hot.cpp |  6 +++++-
+ .../intel_gpu/src/graph/impls/cpu/proposal.cpp       |  1 +
+ .../src/plugin/npuw/partitioning/online/graph.hpp    |  1 +
+ .../intel_npu/src/plugin/npuw/serialization.hpp      |  1 +
+ 15 files changed, 28 insertions(+), 14 deletions(-)
+
+diff --git a/samples/cpp/common/format_reader/src/bmp.cpp b/samples/cpp/common/format_reader/src/bmp.cpp
+index b025e8992f..9d4cae1bc7 100644
+--- a/samples/cpp/common/format_reader/src/bmp.cpp
++++ b/samples/cpp/common/format_reader/src/bmp.cpp
+@@ -5,6 +5,7 @@
+ // clang-format off
+ #include <fstream>
+ #include <iostream>
++#include <cstdint>
+ 
+ #include "bmp.h"
+ // clang-format on
+diff --git a/src/common/util/src/wstring_convert_util.cpp b/src/common/util/src/wstring_convert_util.cpp
+index de76e1134e..a841109276 100644
+--- a/src/common/util/src/wstring_convert_util.cpp
++++ b/src/common/util/src/wstring_convert_util.cpp
+@@ -13,9 +13,9 @@
+ #        include <windows.h>
+ #    endif
+ 
+-#    ifdef __clang__
+-#        pragma clang diagnostic push
+-#        pragma clang diagnostic ignored "-Wdeprecated-declarations"
++#    if defined(__clang__) || defined(__GNUC__)
++#        pragma GCC diagnostic push
++#        pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+ #    endif
+ 
+ std::string ov::util::wstring_to_string(const std::wstring& wstr) {
+@@ -45,8 +45,8 @@ std::wstring ov::util::string_to_wstring(const std::string& string) {
+ #    endif
+ }
+ 
+-#    ifdef __clang__
+-#        pragma clang diagnostic pop
++#    if defined(__clang__) || defined(__GNUC__)
++#        pragma GCC diagnostic pop
+ #    endif
+ 
+ #endif  // OPENVINO_ENABLE_UNICODE_PATH_SUPPORT
+diff --git a/src/core/dev_api/openvino/core/type/nf4.hpp b/src/core/dev_api/openvino/core/type/nf4.hpp
+index 383d3a01b2..685dffcc57 100644
+--- a/src/core/dev_api/openvino/core/type/nf4.hpp
++++ b/src/core/dev_api/openvino/core/type/nf4.hpp
+@@ -6,6 +6,7 @@
+ 
+ #include <array>
+ #include <cmath>
++#include <cstdint>
+ #include <iostream>
+ #include <limits>
+ #include <memory>
+diff --git a/src/core/include/openvino/core/type/bfloat16.hpp b/src/core/include/openvino/core/type/bfloat16.hpp
+index 452239fb0d..be42942742 100644
+--- a/src/core/include/openvino/core/type/bfloat16.hpp
++++ b/src/core/include/openvino/core/type/bfloat16.hpp
+@@ -5,6 +5,7 @@
+ #pragma once
+ 
+ #include <cmath>
++#include <cstdint>
+ #include <iostream>
+ #include <limits>
+ #include <memory>
+diff --git a/src/core/include/openvino/core/type/float16.hpp b/src/core/include/openvino/core/type/float16.hpp
+index 31f7e421dd..05ef2b7fd4 100644
+--- a/src/core/include/openvino/core/type/float16.hpp
++++ b/src/core/include/openvino/core/type/float16.hpp
+@@ -5,6 +5,7 @@
+ #pragma once
+ 
+ #include <cmath>
++#include <cstdint>
+ #include <iostream>
+ #include <limits>
+ #include <memory>
+diff --git a/src/core/include/openvino/core/type/float4_e2m1.hpp b/src/core/include/openvino/core/type/float4_e2m1.hpp
+index b4e9f02992..f21b59df39 100644
+--- a/src/core/include/openvino/core/type/float4_e2m1.hpp
++++ b/src/core/include/openvino/core/type/float4_e2m1.hpp
+@@ -5,6 +5,7 @@
+ #pragma once
+ 
+ #include <cmath>
++#include <cstdint>
+ #include <iostream>
+ #include <limits>
+ #include <memory>
+diff --git a/src/core/include/openvino/core/type/float8_e4m3.hpp b/src/core/include/openvino/core/type/float8_e4m3.hpp
+index 166d0d15e8..ab209cf87f 100644
+--- a/src/core/include/openvino/core/type/float8_e4m3.hpp
++++ b/src/core/include/openvino/core/type/float8_e4m3.hpp
+@@ -5,6 +5,7 @@
+ #pragma once
+ 
+ #include <cmath>
++#include <cstdint>
+ #include <iostream>
+ #include <limits>
+ #include <memory>
+diff --git a/src/core/include/openvino/core/type/float8_e5m2.hpp b/src/core/include/openvino/core/type/float8_e5m2.hpp
+index e9e942bfb8..9c071b4d9d 100644
+--- a/src/core/include/openvino/core/type/float8_e5m2.hpp
++++ b/src/core/include/openvino/core/type/float8_e5m2.hpp
+@@ -5,6 +5,7 @@
+ #pragma once
+ 
+ #include <cmath>
++#include <cstdint>
+ #include <iostream>
+ #include <limits>
+ #include <memory>
+diff --git a/src/core/include/openvino/core/type/float8_e8m0.hpp b/src/core/include/openvino/core/type/float8_e8m0.hpp
+index ffc19134b3..7aabcac0c1 100644
+--- a/src/core/include/openvino/core/type/float8_e8m0.hpp
++++ b/src/core/include/openvino/core/type/float8_e8m0.hpp
+@@ -5,6 +5,7 @@
+ #pragma once
+ 
+ #include <cmath>
++#include <cstdint>
+ #include <iostream>
+ #include <limits>
+ #include <memory>
+diff --git a/src/core/reference/include/openvino/reference/gru_cell.hpp b/src/core/reference/include/openvino/reference/gru_cell.hpp
+index 4998d71cdc..b8129dbb52 100644
+--- a/src/core/reference/include/openvino/reference/gru_cell.hpp
++++ b/src/core/reference/include/openvino/reference/gru_cell.hpp
+@@ -105,14 +105,12 @@ void gru_cell(const T* X,
+                                        reinterpret_cast<char*>(Ht_R_zrh[1].data()),
+                                        reinterpret_cast<char*>(Ht_R_zrh[2].data())};
+ 
+-    size_t num_b_splits = linear_before_reset ? 4 : 3;
++    const size_t num_b_splits = linear_before_reset ? 4 : 3;
+     std::vector<std::vector<T>> biases_zrh(num_b_splits, std::vector<T>(B_shape[0] / num_b_splits));
+-    std::vector<char*> pointers_biases = {reinterpret_cast<char*>(biases_zrh[0].data()),
+-                                          reinterpret_cast<char*>(biases_zrh[1].data()),
+-                                          reinterpret_cast<char*>(biases_zrh[2].data())};
+-    if (linear_before_reset) {
+-        pointers_biases.push_back(reinterpret_cast<char*>(biases_zrh[3].data()));
+-    }
++    std::vector<char*> pointers_biases(num_b_splits);
++    std::transform(biases_zrh.begin(), biases_zrh.end(), pointers_biases.begin(), [](auto&& vec) {
++        return reinterpret_cast<char*>(vec.data());
++    });
+ 
+     // split on gates
+     reference::split(reinterpret_cast<char*>(Xt_W.data()), all_gates_shape, sizeof(T), 1, 3, pointers_XW.data());
+diff --git a/src/plugins/intel_cpu/src/nodes/rnn.cpp b/src/plugins/intel_cpu/src/nodes/rnn.cpp
+index 93d8b521a7..3c4a89af5a 100644
+--- a/src/plugins/intel_cpu/src/nodes/rnn.cpp
++++ b/src/plugins/intel_cpu/src/nodes/rnn.cpp
+@@ -280,7 +280,8 @@ bool RNN::isSupportedOperation(const std::shared_ptr<const ov::Node>& op, std::s
+                               ov::op::v5::GRUSequence::get_type_info_static(),
+                               ov::op::internal::AUGRUCell::get_type_info_static(),
+                               ov::op::internal::AUGRUSequence::get_type_info_static())) {
+-                if (rnnCellBase->get_activations() != std::vector<std::string>{"sigmoid", "tanh"}) {
++                if (const auto activations = std::vector<std::string>{"sigmoid", "tanh"};
++                    rnnCellBase->get_activations() != activations) {
+                     errorMessage = "Not supported activation functions";
+                     return false;
+                 }
+diff --git a/src/plugins/intel_cpu/src/shape_inference/custom/one_hot.cpp b/src/plugins/intel_cpu/src/shape_inference/custom/one_hot.cpp
+index b97b2d47c3..0f9d73721f 100644
+--- a/src/plugins/intel_cpu/src/shape_inference/custom/one_hot.cpp
++++ b/src/plugins/intel_cpu/src/shape_inference/custom/one_hot.cpp
+@@ -21,7 +21,11 @@ Result OneHotShapeInfer::infer(const std::vector<std::reference_wrapper<const Ve
+         OPENVINO_THROW("OneHot depth value can't be negative.");
+     }
+     auto result = input_shapes.front().get();
+-    result.insert(result.begin() + m_axis, depth);
++    auto depth_pos = result.begin();
++    if (result.size() > 0) {
++        depth_pos += m_axis;
++    }
++    result.insert(depth_pos, depth);
+ 
+     return {{std::move(result)}, ShapeInferStatus::success};
+ }
+diff --git a/src/plugins/intel_gpu/src/graph/impls/cpu/proposal.cpp b/src/plugins/intel_gpu/src/graph/impls/cpu/proposal.cpp
+index c53fedba72..d86d6e8f9c 100644
+--- a/src/plugins/intel_gpu/src/graph/impls/cpu/proposal.cpp
++++ b/src/plugins/intel_gpu/src/graph/impls/cpu/proposal.cpp
+@@ -185,6 +185,7 @@ struct im_info_t {
+ 
+ struct proposal_impl : typed_primitive_impl<proposal> {
+     using parent = typed_primitive_impl<proposal>;
++    using parent::execute;
+     using parent::parent;
+ 
+     proposal_impl() : parent() {}
+diff --git a/src/plugins/intel_npu/src/plugin/npuw/partitioning/online/graph.hpp b/src/plugins/intel_npu/src/plugin/npuw/partitioning/online/graph.hpp
+index 3452a7e12a..4e47ee401e 100644
+--- a/src/plugins/intel_npu/src/plugin/npuw/partitioning/online/graph.hpp
++++ b/src/plugins/intel_npu/src/plugin/npuw/partitioning/online/graph.hpp
+@@ -6,6 +6,7 @@
+ #pragma once
+ 
+ #include <any>
++#include <cstdint>
+ #include <functional>
+ #include <iostream>
+ #include <memory>
+diff --git a/src/plugins/intel_npu/src/plugin/npuw/serialization.hpp b/src/plugins/intel_npu/src/plugin/npuw/serialization.hpp
+index 88684dc33f..6cc1d2d6f9 100644
+--- a/src/plugins/intel_npu/src/plugin/npuw/serialization.hpp
++++ b/src/plugins/intel_npu/src/plugin/npuw/serialization.hpp
+@@ -5,6 +5,7 @@
+ #pragma once
+ 
+ #include <array>
++#include <cstdint>
+ #include <functional>
+ #include <iostream>
+ #include <map>
+-- 
+2.43.0
+

commit e2659f2d88443dd7dfc6d60e0bbfd6d39f672202

openvino-inference-engine: Add gtest submodule
The previously added patches need it.

Fixes: e3f1b36af ("openvino-inference-engine : backport patches to fix build issues with gcc-15")
Signed-off-by: Zoltán Böszörményi <zboszor@gmail.com>

recipes-core/opencv/openvino-inference-engine_2025.1.0.bb
@@ -12,6 +12,7 @@ SRC_URI = "git://github.com/openvinotoolkit/openvino.git;protocol=https;name=ope
            git://github.com/opencv/ade.git;protocol=https;destsuffix=git/thirdparty/ade;name=ade;nobranch=1 \
            git://github.com/protocolbuffers/protobuf.git;protocol=https;destsuffix=git/thirdparty/protobuf/protobuf;name=protobuf;branch=main \
            git://github.com/gflags/gflags.git;protocol=https;destsuffix=git/thirdparty/gflags/gflags;name=gflags;nobranch=1 \
+           git://github.com/openvinotoolkit/googletest.git;protocol=https;destsuffix=git/thirdparty/gtest/gtest;name=gtest;nobranch=1 \
            git://github.com/madler/zlib.git;protocol=https;destsuffix=git/thirdparty/zlib/zlib;name=zlib;nobranch=1 \
            git://github.com/openvinotoolkit/mlas.git;protocol=https;destsuffix=git/src/plugins/intel_cpu/thirdparty/mlas;name=mlas;nobranch=1 \
            git://github.com/nodejs/node-api-headers.git;protocol=https;destsuffix=git/node-api-headers-src;name=node-api-headers;nobranch=1 \
@@ -36,6 +37,7 @@ SRCREV_json = "9cca280a4d0ccf0c08f47a99aa71d1b0e52f8d03"
 SRCREV_ade = "0e8a2ccdd34f29dba55894f5f3c5179809888b9e"
 SRCREV_protobuf = "f0dc78d7e6e331b8c6bb2d5283e06aa26883ca7c"
 SRCREV_gflags = "e171aa2d15ed9eb17054558e0b3a6a413bb01067"
+SRCREV_gtest = "99760ac1776430f3df65947992bf4e8ebc0d7660"
 SRCREV_zlib = "51b7f2abdade71cd9bb0e7a373ef2610ec6f9daf"
 SRCREV_mlas = "d1bc25ec4660cddd87804fcf03b2411b5dfb2e94"
 SRCREV_node-api-headers = "186e04b5e40e54d7fd1655bc67081cc483f12488"